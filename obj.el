0cf8902ff226b74eda0605d82f7e33b1bf2ca310
Richard H Lee <ricardohenrylee@gmail.com>
2013-04-09 14:38:19 +0100

    pass test 5

--- obj.el
+++ obj.el
@@ -3,7 +3,8 @@
 
 (defun obj (&rest args)
   (let
-    ((command-or-member))
+    ((return-value)
+    (command-or-member))
     (if args
       (if (hash-table-p (car args))
         (progn
@@ -13,11 +14,12 @@
               (setq command-or-member (car args))
               (cond
                 ((keywordp command-or-member)
-                  ())
+                  (setq return-value (car (cdr (cdr args)))))
                 ((symbolp command-or-member)
                   ())
                 (t (signal 'obj-error
                   "second argument should be keyword symbol or symbol"))))
-            (signal 'obj-error "command or member required after object")))
+            (signal 'obj-error "command or member required after object"))
+          return-value)
         (signal 'obj-error "first argument should ba a hash-table"))
       (make-hash-table))))
