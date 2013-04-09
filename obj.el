6c2df8a2ef795eed0042970234cf66c734af7890
Richard H Lee <ricardohenrylee@gmail.com>
2013-04-09 14:00:27 +0100

    pass test 4 again

--- obj.el
+++ obj.el
@@ -2,13 +2,22 @@
 (put 'obj-error 'error-message "obj error")
 
 (defun obj (&rest args)
-  (if args
-    (if (hash-table-p (car args))
-      (progn
-        (setq args (cdr args))
-        (if args
-          (signal 'obj-error
-            "second argument should be keyword symbol or symbol")
-          (signal 'obj-error "command or member required after object")))
-      (signal 'obj-error "first argument should ba a hash-table"))
-    (make-hash-table)))
+  (let
+    ((command-or-member))
+    (if args
+      (if (hash-table-p (car args))
+        (progn
+          (setq args (cdr args))
+          (if args
+            (progn
+              (setq command-or-member (car args))
+              (cond
+                ((keywordp command-or-member)
+                  ())
+                ((symbolp command-or-member)
+                  ())
+                (t (signal 'obj-error
+                  "second argument should be keyword symbol or symbol"))))
+            (signal 'obj-error "command or member required after object")))
+        (signal 'obj-error "first argument should ba a hash-table"))
+      (make-hash-table))))
