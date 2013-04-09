05da1ea89e79f72f0542a469887338901f405f4d
Richard H Lee <ricardohenrylee@gmail.com>
2013-04-09 12:44:59 +0100

    test 2: obj should be hash table

--- test.el
+++ test.el
@@ -2,8 +2,15 @@
 (load-file "obj.el")
 (ert-delete-all-tests)
 
-(ert-deftest obj-01-new-object-is-hash-table ()
+(ert-deftest obj-test-01-new-object-is-hash-table ()
   (should
     (hash-table-p (obj))))
+
+(ert-deftest obj-test-02-first-argument-is-hash-table ()
+  (should (equal
+    (cdr (should-error
+      (obj nil)
+      :type 'obj-error))
+    "first argument should ba a hash-table")))
 
 (ert t)
