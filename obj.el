a36efe28a9d4348ffc1f9c0e8a27f442b027cc1c
Richard H Lee <ricardohenrylee@gmail.com>
2013-04-09 17:42:21 +0100

    temp

--- obj.el
+++ obj.el
@@ -4,17 +4,23 @@
 (defun obj (&rest args)
   (let
     ((return-value)
-    (command-or-member))
+    (object)
+    (command-or-member)
+    (command)
+    (member))
     (if args
-      (if (hash-table-p (car args))
+      (if (hash-table-p (setq object (car args)))
         (progn
           (setq args (cdr args))
           (if args
             (progn
               (setq command-or-member (car args))
+              (setq args (cdr args))
               (cond
                 ((keywordp command-or-member)
-                  (setq return-value (car (cdr (cdr args)))))
+                  (setq command command-or-member)
+                  (setq member (car args))
+                  (setq return-value (car (cdr args))))
                 ((symbolp command-or-member)
                   ())
                 (t (signal 'obj-error
@@ -23,3 +29,4 @@
           return-value)
         (signal 'obj-error "first argument should ba a hash-table"))
       (make-hash-table))))
+;;after command leave if
