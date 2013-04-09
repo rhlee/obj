645b1718260bf487e4ad93fbdd5b5f6b02690f7c
Richard H Lee <ricardohenrylee@gmail.com>
2013-04-09 12:47:20 +0100

    pass test 2

--- obj.el
+++ obj.el
@@ -1,2 +1,7 @@
+(put 'obj-error 'error-conditions '(error obj-error))
+(put 'obj-error 'error-message "obj error")
+
 (defun obj (&rest args)
-  (make-hash-table))
+  (if args
+    (signal 'obj-error "first argument should ba a hash-table")
+    (make-hash-table)))
