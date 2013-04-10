dea5f5cb98c54213547d3e5c506d48a491893f90
Richard H Lee <ricardohenrylee@gmail.com>
2013-04-10 20:19:19 +0100

    test 12 pass

--- obj.el
+++ obj.el
@@ -1,7 +1,7 @@
 (put 'obj-error 'error-conditions '(error obj-error))
 (put 'obj-error 'error-message "obj error")
 
-(defun signal-obj-error (data)
+(defun signal-obj-error (data command)
   (let* ((errors
     '((first-argument . "first argument should be a hash-table")
       (second-argument .
@@ -12,7 +12,8 @@
 "after member: :set takes 1 value exactly, :get takes no value")))
     (error-assoc (or (assoc data errors)
       '(unspecified-error . "unspecified error"))))
-    (signal 'obj-error (list (car error-assoc) (cdr error-assoc)))))
+    (signal 'obj-error (list (car error-assoc) (cdr error-assoc)
+      (cons 'command command)))))
 
 (defun obj (&rest args)
   (let
@@ -36,23 +37,22 @@
                     (progn
                       (setq member (car args))
                       (setq args (cdr args)))
-                    (signal-obj-error 'member-after-command)))
+                    (signal-obj-error 'member-after-command command)))
                 ((symbolp command-or-member)
                   ())
-                (t (signal-obj-error 'second-argument))))
-            (signal-obj-error 'second-argument))
+                (t (signal-obj-error 'second-argument command))))
+            (signal-obj-error 'second-argument command ))
           (setq value-length (length args))
           (cond
             ((eq command :set)
               (if (eq value-length 1)
                 (puthash :member (car args) object)
-                (signal-obj-error 'value-error)))
+                (signal-obj-error 'value-error command)))
             ((eq command :get)
               (if (eq value-length 0)
                 (gethash :member object)
-                (signal-obj-error 'value-error)))
-            (t (signal-obj-error 'invalid-command))))
-        (signal-obj-error 'first-argument))
+                (signal-obj-error 'value-error command)))
+            (t (signal-obj-error 'invalid-command command))))
+        (signal-obj-error 'first-argument command))
       (make-hash-table))))
 ;;move args down (into cond)
-;;after member
