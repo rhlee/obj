fc3166fd6784e58482e8390bbfcd928532434400
Richard H Lee <ricardohenrylee@gmail.com>
2013-04-11 12:40:28 +0100

    test 15: no command in error if nil

--- test.el
+++ test.el
@@ -123,5 +123,11 @@
       (obj object :get key)
       value))))
 
+(ert-deftest obj-test-15-no-command-in-error-if-nil ()
+  (should (not (assoc 'command
+    (cdr (should-error
+      (obj (obj) nil)
+      :type 'obj-error))))))
+
 (ert t)
 ;;skip command if nil in error
